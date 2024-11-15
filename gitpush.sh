9a9dc8ac1a7344494a44e63145710e347e133ea4
yassinv <156611468+yassinvila@users.noreply.github.com>
2024-11-15 09:13:27 -0500

    first time

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /c/Users/YASS/OneDrive/desktop/ODIN || exit
-git add .
-git commit -m "Update on $(date +'%Y/%m/%d')"
-git push origin main
+git submodule foreach '
+    git add . 
+    git commit -m "Update on $(date +'%Y/%m/%d')"
+    git push origin main 
+'
