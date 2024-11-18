8811f1bbc67a9e9e1efbe19b625e92aa8637b125
yassinv <156611468+yassinvila@users.noreply.github.com>
2024-11-18 13:30:06 -0500

    Update on 2024/11/18

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /c/Users/YASS/OneDrive/Desktop/2024_CSI_127 || exit
+cd /c/Users/YASS/OneDrive/Desktop/ODIN || exit
 git add .
 git commit -m "Update on $(date +'%Y/%m/%d')"
 git push origin main
